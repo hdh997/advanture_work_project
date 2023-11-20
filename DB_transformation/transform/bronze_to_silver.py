6b90b97bdce8102d98b43ff9cb6a08638333f217
hdh997 <hdh997>
2023-11-20 04:31:09 +0000

    transformation

--- DB_transformation/transform/bronze_to_silver.py
+++ DB_transformation/transform/bronze_to_silver.py
@@ -1,2 +1,48 @@
 # Databricks notebook source
+#display(dbutils.fs.ls('mnt/bronze/SalesLT/'))
 
+# COMMAND ----------
+
+#input_path = '/mnt/bronze/SalesLT/Address/'
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Perform tramsformation for all tables
+
+# COMMAND ----------
+
+tbl_name = []
+
+for i in dbutils.fs.ls('mnt/bronze/SalesLT/'):
+    tbl_name.append(i.name.split('/')[0])
+
+# COMMAND ----------
+
+tbl_name
+
+# COMMAND ----------
+
+from pyspark.sql.functions import from_utc_timestamp, date_format
+from pyspark.sql.types import TimestampType
+
+# COMMAND ----------
+
+for name in tbl_name:
+    path = '/mnt/bronze/SalesLT/' + name + '/' + name + '.parquet'
+    df = spark.read.format('parquet').load(path)
+    column = df.columns
+
+    for col in column:
+        if "Date" in col or 'date' in col:
+            df = df.withColumn(col, date_format(from_utc_timestamp(df[col].cast(TimestampType()), 'UTC'), 'dd-MM-yyyy'))
+    output_path = '/mnt/silver/SalesLT/' + name + '/'
+    df.write.format('delta').mode("overwrite").save(output_path)
+
+# COMMAND ----------
+
+#display(df)
+
+# COMMAND ----------
+
+#display(dbutils.fs.ls('mnt/silver/SalesLT/Address'))
--- DB_transformation/transform/silver_to_gold.py
+++ DB_transformation/transform/silver_to_gold.py
@@ -1,2 +1,48 @@
 # Databricks notebook source
+#dbutils.fs.ls('mnt/silver/SalesLT/')
 
+# COMMAND ----------
+
+#dbutils.fs.ls('mnt/gold/')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Perform trasformation for all tables
+
+# COMMAND ----------
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, regexp_replace
+
+# COMMAND ----------
+
+tbl_name = []
+for n in dbutils.fs.ls('mnt/silver/SalesLT/'):
+    tbl_name.append(n.name.split('.')[0])
+
+# COMMAND ----------
+
+tbl_name
+
+# COMMAND ----------
+
+for name in tbl_name:
+    path = '/mnt/silver/SalesLT/' + name
+    df = spark.read.format('delta').load(path)
+
+    col_names = df.columns
+
+    for old_col_name in col_names:
+        #convert from ColumnName to Column_name
+        new_col_name = "".join(["_" + char if char.isupper() and not old_col_name[i-1].isupper() else char for i , char in enumerate(old_col_name)] ).lstrip("_")
+
+        # chnage the column nmae using withColumnRenamed
+        df = df.withColumnRenamed(old_col_name, new_col_name)
+    output_path = '/mnt/gold/SalesLT/' + name + '/'
+    df.write.format('delta').mode('overwrite').save(output_path)
+
+
+# COMMAND ----------
+
+#display(dbutils.fs.ls('mnt/gold/SalesLT/'))
